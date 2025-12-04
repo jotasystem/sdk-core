454655fecbdb7e4ad0278ff39bb3e131968c2bbb
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2025-12-04 16:24:13 -0300

    IIgnoreTenant

---
+++ src/JotaSystem.Sdk.Core/Application/Context/IIgnoreTenant.cs
@@ -0,0 +1,7 @@
+﻿namespace JotaSystem.Sdk.Core.Application.Context
+{
+    /// <summary>
+    /// Marker interface para indicar que o tenant não deve ser validado.
+    /// </summary>
+    public interface IIgnoreTenant { }
+}
