aa62978abe3426d30f1e8a23b3dc19aac9d83459
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2025-12-13 12:29:08 -0300

    BaseValidator

---
+++ src/JotaSystem.Sdk.Core/Application/Validators/BaseValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace JotaSystem.Sdk.Core.Application.Validators
+{
+    public abstract class BaseValidator<T> : AbstractValidator<T>
+    {
+        public virtual Func<object, string, Task<IEnumerable<string>>> ValidateValue =>
+            async (model, propertyName) =>
+            {
+                var context = ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName));
+
+                ValidationResult result = await ValidateAsync(context);
+                if (result.IsValid) return [];
+
+                return result.Errors.Select(e => e.ErrorMessage);
+            };
+    }
+}
