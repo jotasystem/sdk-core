0e2d00f05028df440d83c598ce997328f4276aa1
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2025-12-14 16:02:37 -0300

    JwtParser

---
+++ src/JotaSystem.Sdk.Core/CrossCutting/Security/Jwt/JwtParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace JotaSystem.Sdk.Core.CrossCutting.Security.Jwt
+{
+    public static class JwtParser
+    {
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = Convert.FromBase64String(PadBase64(payload));
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            return keyValuePairs!.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()!));
+        }
+
+        private static string PadBase64(string base64)
+        {
+            return base64.PadRight(base64.Length + (4 - base64.Length % 4) % 4, '=');
+        }
+    }
+}
