64d1c9e1f60d62c8452036718dc9b300fef6fec1
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-07 11:59:33 -0300

    CurrentUser

--- src/JotaSystem.Sdk.Core/CrossCutting/Models/CurrentUser.cs
+++ src/JotaSystem.Sdk.Core/CrossCutting/Models/CurrentUser.cs
@@ -4,21 +4,30 @@ namespace JotaSystem.Sdk.Core.CrossCutting.Models
 {
     public class CurrentUser
     {
-        public Guid UserUuid { get; set; }
+        public long UserId { get; set; }
+        public UserTypeEnum UserType { get; set; }
+
         public long TenantId { get; set; }
-        public long CompanyId { get; set; }
-        public string UserName { get; set; } = string.Empty;
-        public string Nickname { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string Picture { get; set; } = string.Empty;
-        public string TenantImage { get; set; } = string.Empty;
-        public string TenantColor { get; set; } = string.Empty;
-        public UserProfileEnum Profile { get; set; }
-        public List<string> Roles { get; set; } = [];
-        public List<string> Permissions { get; set; } = [];
+        public string TenantImage { get; set; } = default!;
+        public string TenantColor { get; set; } = default!;
+
+        public long? CurrentCompanyId { get; set; }
+        public long? DefaultCompanyId { get; set; }
+        public IReadOnlyCollection<long> AllowedCompanyIds { get; set; } = [];
+
+        public string Username { get; set; } = default!;
+        public string Nickname { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public string Picture { get; set; } = default!;
+        public bool HasFullAccess { get; set; }
+
         public string Culture { get; set; } = string.Empty;
         public string TimeZone { get; set; } = string.Empty;
         public string Currency { get; set; } = string.Empty;
+
+        public IReadOnlyCollection<string> Roles { get; set; } = [];
+        public IReadOnlyCollection<string> Permissions { get; set; } = [];
+
     }
 }
