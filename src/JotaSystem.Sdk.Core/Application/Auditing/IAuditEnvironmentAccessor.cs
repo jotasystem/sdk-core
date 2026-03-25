3dd9f1e72fa6365c8a03692384a1689ca7e08986
joao.bosqueti <joao.bosqueti@programaconsumer.com.br>
2026-03-25 16:45:43 -0300

    IAuditEnvironmentAccessor

---
+++ src/JotaSystem.Sdk.Core/Application/Auditing/IAuditEnvironmentAccessor.cs
@@ -0,0 +1,14 @@
+﻿namespace JotaSystem.Sdk.Core.Application.Auditing
+{
+    public interface IAuditEnvironmentAccessor
+    {
+        long? GetUserId();
+        string? GetUsername();
+        long? GetCompanyId();
+        long? GetTenantId();
+        string? GetCorrelationId();
+        string? GetIpAddress();
+        string? GetUserAgent();
+        string? GetSource();
+    }
+}
