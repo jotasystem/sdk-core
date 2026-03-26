ed784b83c97a186c21895437097ba2444a8fc3c2
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-26 12:53:44 -0300

    IAuditableRequest

--- src/JotaSystem.Sdk.Core/Application/Auditing/IAuditableRequest.cs
+++ src/JotaSystem.Sdk.Core/Application/Auditing/IAuditableRequest.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace JotaSystem.Sdk.Core.Application.Auditing
+﻿namespace JotaSystem.Sdk.Core.Application.Auditing
 {
     public interface IAuditableRequest
     {
-        AuditDefinition GetAuditDefinition();
-        AuditPayload? GetAuditPayload();
+        AuditDefinition Audit();
     }
 }
